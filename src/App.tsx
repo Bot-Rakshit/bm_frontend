e1e4e3a6ebc22373b3ca0f3a398138292836bc5c
Bot-Rakshit <singhrakshit003@gmail.com>
2024-06-25 18:42:18 +0530

    fixed error logs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-
-import ReactSVG from '@/assets/react.svg';
+import BMLogo from '@/assets/bm_samay_logo.svg';
 import { Badge } from '@/components/ui/badge';
 
 function App() {
@@ -7,10 +6,12 @@ function App() {
     <main className="flex flex-col items-center justify-center h-screen">
       <div className="flex flex-col items-center gap-y-4">
         <div className="inline-flex items-center gap-x-4">
-          <img src={ReactSVG} alt="React Logo" className="w-32" />
+          <img src={BMLogo} alt="BM Samay Logo" className="w-32" />
           <span className="text-6xl">+</span>
           <img src={'/vite.svg'} alt="Vite Logo" className="w-32" />
         </div>
+        <h1 className="text-4xl font-bold">Welcome to BM Samay</h1>
+        <p className="text-lg text-center">Join the BM SamayRaina community and get noticed by Samay Raina. Earn BM points, make predictions, and get discounted offers on Samay's merch as well as a chance to win free show tickets.</p>
         <a href="https://ui.shadcn.com" rel="noopener noreferrer nofollow" target="_blank">
           <Badge variant="outline">shadcn/ui</Badge>
         </a>
