707ea506e3a7e94cbbb382623b4142f1a1e7f60c
Bot-Rakshit <singhrakshit003@gmail.com>
2024-06-27 20:01:07 +0530

    unused import

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import BMSamayLogo from '../assets/SamayBM.png';
