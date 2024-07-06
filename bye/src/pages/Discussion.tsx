bf3eee5617197bf26011cc501a8a9f0dbd68634c
SambhavSetia <sambhavsetia29@gmail.com>
2024-07-06 20:10:50 +0530

    Add chat page

--- bye/src/pages/Discussion.tsx
+++ bye/src/pages/Discussion.tsx
@@ -21,14 +21,14 @@ import  { useState, ChangeEvent, FormEvent } from 'react';
   return (
     <div className="  bg-gradient-to-br from-[#2f8d2f] to-[#0a1f0a] rounded-3xl p-8 shadow-xl transform hover:scale-105 transition-transform duration-300 border border-neon-green/20  min-h-screen flex flex-col items-center justify-center   ">
       <header className="w-full max-w-md rounded-xl p-6 bg-gradient-to-br from-[#1a3a1a] to-[#0f2e0f] border border-neon-green/20 shadow-lg hover:shadow-neon-green/20 transition-all duration-300">
-        <h1 className="text-3xl font-bold mb-4 text-center text-white">BM Suggestion Box</h1>
+        <h1 className="text-3xl font-bold mb-4 text-center text-white">BM Chat Box</h1>
         <form onSubmit={handleSubmit} className="mb-4">
           <input
             type ="text "
             value={input}
             onChange={handleInputChange}
             className="w-full p-3 border rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-blue-400 text-black"
-            placeholder="Enter your suggestion"
+            placeholder="Enter your Chat"
           />
           <button type="submit" className="w-full bg-neon-green text-black p-3 rounded-md hover:bg-green-600 transition duration-300">
             Submit
