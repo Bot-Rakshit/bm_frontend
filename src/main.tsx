c639adff988b2d2ed98900f72b5d8c9ba9d6058f
Bot-Rakshit <singhrakshit003@gmail.com>
2024-07-09 18:23:06 +0530

    remove redudant code

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import App from './App.tsx';
 import { LandingPage } from './pages/landing.tsx';
 import Signup from './pages/signup.tsx';
 import '@/styles/globals.css';
-import Test from './pages/test.tsx';
 import SignUpCallback from './pages/signupcallback.tsx';
 import Welcome from './pages/welcome.tsx';
 import Community from './pages/community.tsx';
@@ -21,7 +20,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
           <Route path="/" element={<LandingPage />} />
           <Route path="/app" element={<App />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/test" element={<Test />} />
           <Route path="/welcome" element={<Welcome />} />
           <Route path="/community" element={<Community />} />
           <Route path="/signupcallback" element={<SignUpCallback />} />
