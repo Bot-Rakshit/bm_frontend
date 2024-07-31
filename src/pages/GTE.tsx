c1a04a4a5d18a06ca215a0fe5bf3678caab93b40
vatan malik <vmro45@gmail.com>
2024-07-31 14:52:58 +0530

    fixedd compress and expand for both mobile and dekstop + header

--- src/pages/GTE.tsx
+++ src/pages/GTE.tsx
@@ -12,6 +12,7 @@ import Sidebar from '@/components/sidebar/Sidebar';
 import { fetchRandomGame } from '@/services/gamefetcher';
 import noobAvatar from '@/assets/noob.jpg';
 import bmAvatar from '@/assets/bm.jpg';
+import Header from '@/components/sidebar/Header';
 
 const GuessTheElo: React.FC = () => {
   const [game, setGame] = useState(new Chess());
@@ -166,6 +167,10 @@ const GuessTheElo: React.FC = () => {
     <div className="flex h-screen">
       <Sidebar />
       <div className="flex-1 flex flex-col relative w-full">
+      <Header
+      headerTitle="Guess The Elo"
+      showChesscom
+    />
         <Background />
         <main className="flex-1 p-6 md:p-10 z-10 overflow-y-auto">
           <motion.div
@@ -229,7 +234,10 @@ const GuessTheElo: React.FC = () => {
                         onClick={toggleMoveTable}
                         className="bg-gray-700 hover:bg-gray-600 text-neon-green font-bold py-2 px-4 rounded"
                       >
-                        {showMoveTable ? <FaCompressAlt /> : <FaExpandAlt />}
+                      {isMobile 
+                        ? (showMoveTable ? <FaCompressAlt /> : <FaExpandAlt />)
+                        : (showMoveTable ? <FaExpandAlt /> : <FaCompressAlt />)
+                      }
                       </Button>
                     </div>
                   </div>
